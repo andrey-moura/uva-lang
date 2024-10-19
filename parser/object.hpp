3c1a0119c224958bd223708854a375a4bc178ea6
Andrey Moura <andreymds88@gmail.com>
2024-10-19 19:53:39 -0300

    wip: move object to specific header file

---
+++ parser/object.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+#include <memory>
+
+namespace uva
+{
+    namespace lang {
+        struct object
+        {
+            object(std::shared_ptr<uva::lang::structure> c) : cls(c) {};
+            ~object();
+            std::shared_ptr<uva::lang::structure> cls;
+            void* native = nullptr;
+            std::shared_ptr<object> base_instance = nullptr;
+            object* derived_instance = nullptr;
+        };
+    };
+};
