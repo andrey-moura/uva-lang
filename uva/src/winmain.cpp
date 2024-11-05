dfbbc607949e215f03b2616caf78b788d77d7c4a
Andrey Moura <andreymds88@gmail.com>
2024-11-05 12:00:14 -0300

    wip: fix typo

--- uva/src/winmain.cpp
+++ uva/src/winmain.cpp
@@ -2,7 +2,7 @@
 #ifdef _WIN32  // Check if Windows
     #include <windows.h>
 
-    extern int uva_main(int argc, char *argv[]);
+    extern int uva_main(int argc, char** argv);
 
     int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
         return uva_main(0, nullptr);
