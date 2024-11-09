efb58b5e57c3498a0735b25a7958b519d5772939
Andrey Moura <andreymds88@gmail.com>
2024-11-09 13:56:18 -0300

    internal: Merge all classes into class.cpp to increase compilation time

--- lang/src/class.cpp
+++ lang/src/class.cpp
@@ -2,6 +2,16 @@
 
 #include <console.hpp>
 
+#include "false_class.cpp"
+#include "true_class.cpp"
+#include "string_class.cpp"
+#include "integer_class.cpp"
+#include "file_class.cpp"
+#include "std_class.cpp"
+#include "array_class.cpp"
+#include "null_class.cpp"
+#include "dictionary_class.cpp"
+
 uva::lang::structure::structure(const std::string& __name, std::vector<uva::lang::method> __methods)
     : name(__name)
 {
