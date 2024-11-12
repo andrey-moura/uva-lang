ec292b5952570d29401cb0642d08f01f513abdc5
Andrey Moura <andreymds88@gmail.com>
2024-11-12 13:56:37 -0300

    wip: Removed unused code

--- lexer/src/lexer.cpp
+++ lexer/src/lexer.cpp
@@ -1,18 +1,5 @@
 #include <lexer.hpp>
 
-// std::vector<std::pair<std::string_view, uva::lang::lexer::cursor_type>> uva::lang::lexer::cursor_type_from_string_map = {
-//     /* Keep in most used order */
-
-//     { "//",       cursor_type::cursor_comment          },
-//     { "var",      cursor_type::cursor_var              },
-//     { "{",        cursor_type::cursor_block            },
-//     { "function", cursor_type::cursor_function         },
-//     { "return",   cursor_type::cursor_return           },
-//     { "class",    cursor_type::cursor_class            },
-//     { "extends",  cursor_type::cursor_baseclass        },
-//     { "require_directory", cursor_type::cursor_fncall  },
-// };
-
 bool is_delimiter(const char& c) {
     static std::vector<bool> delimiters;
     if(delimiters.empty()) {
