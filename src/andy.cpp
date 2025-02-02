8a5f7f3ca4e618cb6bc206db0520645d49209548
Andrey Moura <andreymds88@gmail.com>
2025-02-01 23:51:06 -0300

    fix: Andy was always trying to open utility

--- src/andy.cpp
+++ src/andy.cpp
@@ -33,29 +33,31 @@ int main(int argc, char** argv) {
         if(argc > 1) {
             std::string_view arg = argv[1];
 
-            if(arg == "--help") {
-                std::cout << "Usage: " << argv[0] << " [file]" << std::endl;
-                std::cout << std::endl;
-                std::cout << "Options: " << std::endl;
-                uva::console::print_warning("  --help");
-                std::cout << "     Display this information" << std::endl;
-                uva::console::print_warning("  --version");
-                std::cout << "  Display the version of the andy language" << std::endl;
-                return 0;
-            } else if(arg == "--version") {
-                std::cout << ANDYLANG_VERSION << std::endl;
-                return 0;
-            } else {
-                arg.remove_prefix(2);
-                file_path = andy::lang::config::src_dir() / "utility" / arg;
-                file_path.replace_extension(".andy");
+            if(arg.starts_with("--")) {
+                if(arg == "--help") {
+                    std::cout << "Usage: " << argv[0] << " [file]" << std::endl;
+                    std::cout << std::endl;
+                    std::cout << "Options: " << std::endl;
+                    uva::console::print_warning("  --help");
+                    std::cout << "     Display this information" << std::endl;
+                    uva::console::print_warning("  --version");
+                    std::cout << "  Display the version of the andy language" << std::endl;
+                    return 0;
+                } else if(arg == "--version") {
+                    std::cout << ANDYLANG_VERSION << std::endl;
+                    return 0;
+                } else {
+                    arg.remove_prefix(2);
+                    file_path = andy::lang::config::src_dir() / "utility" / arg;
+                    file_path.replace_extension(".andy");
 
-                if(!std::filesystem::exists(file_path)) {
-                    throw std::runtime_error("utility does not exist");
-                }
+                    if(!std::filesystem::exists(file_path)) {
+                        throw std::runtime_error("utility does not exist");
+                    }
 
-                if(!std::filesystem::is_regular_file(file_path)) {
-                    throw std::runtime_error("utility is not a regular file");
+                    if(!std::filesystem::is_regular_file(file_path)) {
+                        throw std::runtime_error("utility is not a regular file");
+                    }
                 }
             }
         }
