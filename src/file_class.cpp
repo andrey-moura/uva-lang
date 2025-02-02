2caf19cf74b5c651fcc3b4d982d614d4ea9f73f4
Andrey Moura <andreymds88@gmail.com>
2025-02-01 22:40:58 -0300

    internal: File.read now accpets string or Path

--- src/file_class.cpp
+++ src/file_class.cpp
@@ -12,13 +12,15 @@ std::shared_ptr<andy::lang::structure> andy::lang::file_class::create(andy::lang
 
     FileClass->methods = {
         { "read", andy::lang::method("read", method_storage_type::class_method, {"path"}, [interpreter](std::shared_ptr<andy::lang::object> object, std::vector<std::shared_ptr<andy::lang::object>> params) {
-            const std::string& input_path = params[0]->as<std::string>();
-            std::filesystem::path path = std::filesystem::absolute(input_path);
-
-            if(!std::filesystem::exists(path)) {
-                throw std::runtime_error("file '" + path.string() + "' does not exist");
+            std::filesystem::path path;
+            std::shared_ptr<andy::lang::object> path_object = params[0];
+            if(path_object->cls == interpreter->StringClass) {
+                path = path_object->as<std::string>();
+            } else if(path_object->cls == interpreter->PathClass) {
+                path = path_object->as<std::filesystem::path>();
+            } else {
+                throw std::runtime_error("invalid path");
             }
-
             return andy::lang::object::instantiate(interpreter, interpreter->StringClass, std::move(uva::file::read_all_text<char>(path)));
         })},
         { "read_all_lines", andy::lang::method("read_all_lines", method_storage_type::class_method, {"path"}, [interpreter](std::shared_ptr<andy::lang::object> object, std::vector<std::shared_ptr<andy::lang::object>> params) {
